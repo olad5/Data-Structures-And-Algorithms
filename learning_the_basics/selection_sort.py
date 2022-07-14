24c522c37db0cbff1eb0bd775ae838e315343a71
Francis Shonubi <shonubioladipo@gmail.com>
2022-07-14 17:05:55 +0100

    feat: add selection sort algorithm

---
+++ learning_the_basics/selection_sort.py
@@ -0,0 +1,16 @@
+
+def selection_sort(my_list):
+    for i in range(len(my_list) - 1):
+        min_index  = i
+        for j in range(i+1, len(my_list)):
+            if my_list[j] < my_list[min_index] :
+                min_index = j
+        if i != min_index:
+                temp = my_list[i]
+                my_list[i] = my_list[min_index]
+                my_list[min_index] = temp
+    return my_list
+
+
+if __name__ == "__main__":
+    print(selection_sort([4,2,6,5,1,3]))
