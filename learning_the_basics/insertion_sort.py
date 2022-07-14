a9e0506b275639eaf9f5c6cd47ccebe0190322c1
Francis Shonubi <shonubioladipo@gmail.com>
2022-07-14 20:45:09 +0100

    feat: add insertion sort algorithm

---
+++ learning_the_basics/insertion_sort.py
@@ -0,0 +1,14 @@
+
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp  = my_list[i]
+        j = i - 1
+        while temp < my_list[j] and j > -1:
+            my_list[j+1] = my_list[j] 
+            my_list[j] = temp
+            j -= 1
+    return my_list
+
+
+if __name__ == "__main__":
+    print(insertion_sort([4,2,6,5,1,3]))
